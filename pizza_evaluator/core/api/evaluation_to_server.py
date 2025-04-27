3bc708d2a87af0d20c68049b712667b041dd96df
himozaa <impocanal@gmail.com>
2025-04-27 19:46:03 +0500

    make starting sending in a separate thread

--- pizza_evaluator/core/api/evaluation_to_server.py
+++ pizza_evaluator/core/api/evaluation_to_server.py
@@ -4,6 +4,7 @@ import io
 import requests
 import json
 import numpy as np
+import threading
 import cv2
 
 
@@ -13,29 +14,32 @@ def send_evaluation_to_server(
     ingredients: list,
     photo: np.ndarray
 ):
-    # Кодируем в JPEG (или PNG)
-    success, encoded_image = cv2.imencode(".jpg", photo)
-    if not success:
-        raise ValueError("Ошибка кодирования изображения")
+    def task():
+        # Кодируем в JPEG (или PNG)
+        success, encoded_image = cv2.imencode(".jpg", photo)
+        if not success:
+            raise ValueError("Ошибка кодирования изображения")
 
-    # Оборачиваем как файл
-    image_bytes = io.BytesIO(encoded_image.tobytes())
-    image_bytes.name = "pizza.jpeg"  # важно, иначе Django может не распознать как файл
+        # Оборачиваем как файл
+        image_bytes = io.BytesIO(encoded_image.tobytes())
+        image_bytes.name = "pizza.jpeg"  # важно, иначе Django может не распознать как файл
 
-    files = {
-        'photo': image_bytes
-    }
+        files = {
+            'photo': image_bytes
+        }
 
-    data = {
-        'token': os.getenv("PIZZERIA_TOKEN"),
-        'pizza_id': pizza_id,
-        'crust_percentage': crust_percentage,
-        'ingredients': json.dumps(ingredients)
-    }
+        data = {
+            'token': os.getenv("PIZZERIA_TOKEN"),
+            'pizza_id': pizza_id,
+            'crust_percentage': crust_percentage,
+            'ingredients': json.dumps(ingredients)
+        }
 
-    response = requests.post(os.getenv("API_URL"), data=data, files=files)
+        try:
+            response = requests.post(os.getenv("API_URL"), data=data, files=files)
+            print("Ответ:", response.json())
+        except Exception:
+            print("Ошибка ответа:", response.status_code, response.text)
 
-    try:
-        print("Ответ:", response.json())
-    except Exception:
-        print("Ошибка ответа:", response.status_code, response.text)
+    # Запускаем отправку в фоне
+    threading.Thread(target=task, daemon=True).start()
